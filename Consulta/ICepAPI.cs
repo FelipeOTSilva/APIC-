ea5f4b643718d0c320d3e279a079c379cae8fbbb
Felipe Silva <lips1407@gmail.com>
2021-06-19 17:11:43 -0300

    implementaçao

---
+++ Consulta/ICepAPI.cs
@@ -0,0 +1,10 @@
+﻿using Refit;
+using System.Threading.Tasks;
+namespace Consulta
+{
+    public interface ICepAPI
+    {
+        [Get ("/ws/{cep}/json")]
+        Task<CEPClass> GetAddressASync(string cep);
+    }
+}
---
+++ Consulta/Program.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace Consulta
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try 
+            {
+                var cep = RestService.For<ICepAPI>("https://viacep.com.br/");
+                Console.WriteLine("Informe seu CEP:");
+
+                string CepInformado = Console.ReadLine().ToString();
+
+                var Consulta = await cep.GetAddressASync(CepInformado);
+
+                Console.WriteLine($"\n Endereço:{Consulta.Logradouro} \n Complemento: {Consulta.Complemento} \n Bairro:{Consulta.Bairro} " +
+                    $"\n Cidade:{Consulta.Localidade} \n Estado: {Consulta.UF}");
+                Console.ReadKey();
+            
+            } catch { Exception e; }
+            {
+                Console.WriteLine("Erro na consulta do CEP!");
+            }
+        }
+    }
+}
